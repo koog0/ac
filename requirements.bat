f24885e17292876925b2712bcfe8e18b8e3d507c
koog0 <168228440+koog0@users.noreply.github.com>
2025-03-11 11:18:21 +0100

    Update requirements.bat

--- requirements.bat
+++ requirements.bat
@@ -1,24 +1,46 @@
 @echo off
+
 :: BatchGotAdmin
+
 :-------------------------------------
+
 REM  --> Check for permissions
+
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 
+
+
 REM --> If error flag set, we do not have admin.
+
 if '%errorlevel%' NEQ '0' (
+
     goto UACPrompt
+
 ) else ( goto gotAdmin )
 
+
+
 :UACPrompt
+
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+
     echo UAC.ShellExecute "cmd.exe", "/c ""%~s0""", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
     "%temp%\getadmin.vbs"
+
     exit /B
 
+
+
 :gotAdmin
+
     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+
     pushd "%CD%"
+
     CD /D "%~dp0"
+
 :--------------------------------------
 
-start pip install os
+start pip install keyboard
+start pip install pynput
