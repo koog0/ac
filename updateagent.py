6ddcb538e49e2a66471f69b423d4c40ce2e0c5a4
koog0 <168228440+koog0@users.noreply.github.com>
2025-03-11 13:47:25 +0100

    Add files via upload

---
+++ updateagent.py
@@ -0,0 +1,55 @@
+import requests
+import os
+import os.path
+import json
+import base64
+
+endpoint = "https://raw.githubusercontent.com/koog0/ac/refs/heads/main/"
+
+def remove_trailing_empty_lines(text):
+    lines = text.splitlines()
+    
+    while lines and not lines[-1].strip():
+        lines.pop()
+    
+    return '\n'.join(lines)
+
+def update_launcher():
+    try:
+        response = requests.get(endpoint + "ezacf.py")
+        
+        gitCode = response.text
+        gitCode = remove_trailing_empty_lines(gitCode)
+
+        with open("ezacf.py", "r") as f:
+            userCode = f.read()
+            f.close()
+
+            userCode = remove_trailing_empty_lines(userCode)
+        
+        if userCode != gitCode:
+            with open("ezacf.py", "w") as f:
+                f.write(gitCode)
+                f.close()
+    except:
+        None
+
+def update_program():
+    try:
+        response = requests.get(endpoint + "ezacf.pyw")
+        
+        gitCode = response.text
+        gitCode = remove_trailing_empty_lines(gitCode)
+
+        with open("ezacf.pyw", "r") as f:
+            userCode = f.read()
+            f.close()
+
+            userCode = remove_trailing_empty_lines(userCode)
+        
+        if userCode != gitCode:
+            with open("ezacf.pyw", "w") as f:
+                f.write(gitCode)
+                f.close()
+    except:
+        None
